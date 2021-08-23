dc21fc1def8b09bcd19ed4b69da2cf0a3a30fa82
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2021-08-23 19:16:19 +0200

    Add player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButton(0))
+        {
+            if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Ground"))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+
+}
