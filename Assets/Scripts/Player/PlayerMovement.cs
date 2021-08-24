b199246482c7a583a767cc42bf3d19ee269641dc
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2021-08-24 09:26:57 +0200

    Add enemy field of view

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,12 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
     private NavMeshAgent agent;
-    public Transform target;
 
     // Start is called before the first frame update
     void Start()
