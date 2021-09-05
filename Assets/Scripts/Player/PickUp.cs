bae17aeedefa6a684d2c65fced5b9e9b77ad26a0
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2021-09-05 15:42:50 +0200

    Add pick up distance

--- Assets/Scripts/Player/PickUp.cs
+++ Assets/Scripts/Player/PickUp.cs
@@ -1,23 +1,32 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 public class PickUp : MonoBehaviour
 {
     [SerializeField]
     private Inventory inventory;
+    private NavMeshAgent agent;
+    private GameObject itemTarget;
+    private GameObject itemToSave;
+    private Animator animator;
 
     void Start()
     {
         inventory = GetComponent<Inventory>();
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
+        WalkToitem();
         PickUpItem();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Gold"))
+        if (other.gameObject.CompareTag("Gold") && !EventSystem.current.IsPointerOverGameObject())
         {
             inventory.gold += other.gameObject.GetComponent<Gold>().goldAmount;
             inventory.UpdateGoldText();
@@ -25,18 +34,47 @@ public class PickUp : MonoBehaviour
         }
     }
 
-    private void PickUpItem()
+    private void WalkToitem()
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Input.GetMouseButton(0))
         {
-            if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Item"))
+            if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("Item") && !EventSystem.current.IsPointerOverGameObject())
             {
-                GameObject item = hit.transform.gameObject.GetComponent<Item>().itemPrefab.item;
-                inventory.AddItem(item);
-                Destroy(hit.transform.gameObject);
+                agent.destination = hit.transform.position;
+                itemToSave = hit.transform.gameObject.GetComponent<Item>().itemPrefab.item;
+                itemTarget = hit.transform.gameObject;
+                animator.SetBool("isWalking", true);
+            }
+            else
+            {
+                itemTarget = null;
             }
         }
+    }
+
+    private void PickUpItem() 
+    {
+        if (itemTarget != null && IsAtTarget())
+        {
+            inventory.AddItem(itemToSave);
+            Destroy(itemTarget);
+        }
+    }
+
+    private bool IsAtTarget()
+    {
+        if (itemTarget != null)
+        {
+            float dist = Vector3.Distance(itemTarget.transform.position, transform.position);
+            Debug.Log(dist);
+            if (dist <= 0.5)
+            {
+                
+                return true;
+            }
+        }
+        return false;
     }
 }
